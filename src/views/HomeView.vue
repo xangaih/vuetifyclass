b3891337d6919878834f8f23161c75c382caa2b1
Khangai Enkhbat <125935784+xangaih@users.noreply.github.com>
2025-03-23 19:38:21 -0400

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <v-app>
+    <v-container>
+      <v-card
+        
+        title="Khangai Enkhbat"
+        subtitle="Junior @ DePauw University"
+        text="..."
+        variant="outlined"
+      >
+        <v-card-actions>
+          <v-btn >Click me</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+
+
+<script setup>
+
+</script>
